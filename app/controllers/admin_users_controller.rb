adcf49dbcc99f8aa6beef34bc524b471fe9d0f4d
ryo isokawa <rio.k.haneuma@gmail.com>
2020-11-24 03:32:00 +0000

    made admin user登録用フォーム

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,9 +1,28 @@
 class AdminUsersController < ApplicationController
   def new
+    @admin_user = AdminUser.new
+  end
+  def create
+    @amdin_user = AdminUser.new(admin_user_params)  # (params[:admin_user])　これは完璧でない実装だけど・・・
+                                                    # paramsの:admin_userってどこで定義してる？？
+    if @admin_user.save # 保存が成功したら
+    
+    else # 保存できないならnewに戻る
+      render 'new'
+    end
   end
   def home
   end
   def show
     @admin_user = AdminUser.find(params[:id])
   end
+  
+  private
+
+    def admin_user_params
+      params.require(:admin_user).permit(:name, :email, :password, :password_confirmation)
+      # 受け取る値はpernmitの値のみ、他の値は受け取らないようにする。
+    end
+
+
 end
