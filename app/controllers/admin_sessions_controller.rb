35ed19f368eb2d99a903b0a158b407fe8b5f68ac
ryo isokawa <rio.k.haneuma@gmail.com>
2020-11-28 00:18:55 +0000

    node？bootstrap効かない　エラーなおす！！

---
+++ app/controllers/admin_sessions_controller.rb
@@ -0,0 +1,16 @@
+class AdminSessionsController < ApplicationController
+  def new
+  end
+  def create
+    admin_user = AdminUser.find_by(email: params[:session][:email].downcase) # AdminUserからemailのみで検索
+    if admin_user && admin_user.authenticate(params[:session][:password]) 
+      # admin_userがnilでないかつパスワードが一致した場合
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      admin_log_in admin_user
+      redirect_to admin_user
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードが一致しません'
+      render 'new'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   root to: 'admin_users#home'
   
+  get    '/admin_login',   to: 'admin_sessions#new'
+  post   '/admin_login',   to: 'admin_sessions#create'
+  delete '/admin_logout',  to: 'admin_sessions#destroy'
+  
   resources :admin_users
   
 end
